9ca1d91f528e1db2f24cc06736f2f8a61806b893
Bryan Guillen <bryanguillenlds@gmail.com>
2020-08-22 22:17:09 -0600

    Test

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,5 @@
 const monk = require('monk');
-const connectionString = 'mongodb+srv://admin-bryan:bryanpass@cluster0-mnqtb.mongodb.net/message-board?retryWrites=true&w=majority';
+const connectionString = 'mongodb+srv://admin-bryan:notmypass@cluster0-mnqtb.mongodb.net/message-board?retryWrites=true&w=majority';
 
 const db = monk(connectionString);
 
